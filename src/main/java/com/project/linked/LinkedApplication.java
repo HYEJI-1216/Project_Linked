ec858945c766953c83568fb876db83c79124ab0e
HYEJI-1216 <rosemary41322@gmail.com>
2026-05-22 15:49:36 +0900

    프로젝트 파일 업로드

---
+++ src/main/java/com/project/linked/LinkedApplication.java
@@ -0,0 +1,13 @@
+package com.project.linked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/linked/LinkedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.linked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
